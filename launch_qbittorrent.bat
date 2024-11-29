2a42357fe650cf101758599e8ea8a36a3e67bae7
Dylan Kinnett <dkinnett@thewalters.org>
2024-11-29 10:18:42 -0500

    Update to apps installed via scoop

--- launch_qbittorrent.bat
+++ launch_qbittorrent.bat
@@ -5,9 +5,9 @@
 @ECHO OFF
 
 :: if closed, open qBittorrent
-wmic process where "name='qbittorrent.exe'" get ProcessID | find /i "ProcessId" > nul || (START "qBittorrent" /D "C:\Program Files\qBittorrent\" qbittorrent.exe)
+wmic process where "name='qbittorrent.exe'" get ProcessID | find /i "ProcessId" > nul || (START "qBittorrent" /D "C:\Users\dylan\scoop\apps\qbittorrent\current" qbittorrent.exe)
 
 :: if closed, open JackettTray
-wmic process where "name='JackettConsole.exe'" get ProcessID | find /i "ProcessId" > nul || (START "Jackett" /D "C:\Users\dylan\scoop\apps\jackett\0.22.820\" JackettTray.exe)
+wmic process where "name='JackettConsole.exe'" get ProcessID | find /i "ProcessId" > nul || (START "Jackett" /D "C:\Users\dylan\scoop\apps\jackett\current" JackettTray.exe)
 
 EXIT
